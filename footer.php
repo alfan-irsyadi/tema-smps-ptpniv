a2b9147bb87d219a442415ebf1b180556b54a25c
alfan-irsyadi <irsyadialfan2@gmail.com>
2022-08-10 17:15:17 +0700

    ubah posisi script.js

--- footer.php
+++ footer.php
@@ -2,5 +2,9 @@
 <h1>FOOTER</h1>
 </div>
 </div>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/script.js'; ?>">
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,10 +1,6 @@
 <html>
 <head>
 <title>Tutorial theme</title>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/script.js'; ?>">
-</script>
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
