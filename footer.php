93764c73b834b46d4d328628b9fae654c699d8b4
astridfadillah <astridaqila1314@gmail.com>
2022-08-13 21:02:24 +0700

    fixfooter astrid

--- footer.php
+++ footer.php
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<html>
-  <head>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-  </head>
-<body>
-  <footer>
-    <div class="content">
-      <div class="link-boxes">
-        <ul class="box">
-          <li class="link_name">Hubungi Kami</li>
-          <li class="media-icons">
-            <p><i class="fab fa-solid fa-phone">0821-****-*** (Nama)</i></p>
-          </li> 
-        </ul>
-        <ul class="box">
-          <li class="link_name">Lokasi</li>
-          <li><a href="https://maps.app.goo.gl/3GFsENXKZ7L8wB9q8">Jl. Yogyakarta, Kecamatan Dolok Batu Nanggar, Kabupaten Simalungun, Sumatera Utara</a></li>
-        </ul>
-        <ul class="box">
-=======
 <footer>
       <div class="content">
         <div class="link-boxes">
@@ -34,7 +12,6 @@
             <li><a href="https://goo.gl/maps/J3HC4ERRvGgJizjb7">Moho, Kec. Jawa Maraja Bah Jambi, Kabupaten Simalungun, Sumatera Utara 21174</a></li>
           </ul>
           <ul class="box">
->>>>>>> d8944d5791f43131a6b0d6df88619f2c694456eb
             <li class="link_name">Media Sosial</li>
             <li class="media-icons">
               <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -45,16 +22,10 @@
         </div>
       </div>
 
-<<<<<<< HEAD
-    <div class="bottom-details">
-      <div class="bottom_text">
-        <span class="copyright_text">Copyright © 2022 <a href="#">Tim PPM USU 2022.</a>All rights reserved</span>
-=======
       <div class="bottom-details">
         <div class="bottom_text">
           <span class="copyright_text">Copyright © 2022 <a href="#">SMPS PTPN IV Dolok Ilir.</a>All rights reserved</span>
         </div>
->>>>>>> d8944d5791f43131a6b0d6df88619f2c694456eb
       </div>
     </footer>
     <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri() . '/js/bootstrap.js'; ?>"></script>
