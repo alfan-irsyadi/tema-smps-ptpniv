f7ffea281568fc4cc66a1a4678fdd0e432928c94
alfan-irsyadi <irsyadialfan2@gmail.com>
2022-08-13 20:49:30 +0700

    fix conclict

--- header.php
+++ header.php
@@ -2,24 +2,12 @@
 <html>
 
 <head>
-<<<<<<< HEAD
-<title>Tutorial theme</title>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
-</script>
-<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-=======
   <title>SMPS PTPN IV</title>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/css/bootstrap.css'; ?>">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
->>>>>>> d8944d5791f43131a6b0d6df88619f2c694456eb
 </head>
 
 <body>
